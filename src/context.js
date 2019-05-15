f3a65d02ede58513fe5fe0c92f260112c9c6f235
RafaelaSilveira <rafaelacss21@gmail.com>
2019-05-15 19:45:30 -0400

    add single room template 2:50

--- src/context.js
+++ src/context.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
+import items from "./data";
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
   state = {
-    greeting: "welcome to beach resort",
-    name: "john"
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+  //get data
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    // console.log(rooms);
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRoom: rooms,
+      loading: false
+    });
+  }
+
+  formatData(items) {
+    let tempItems = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url);
+      let room = { ...item.fields, images, id };
+      return room;
+    });
+    return tempItems;
+  }
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
+    return room;
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function About() {
-  return <div>Hello from About page!</div>;
+  return (
+    <Hero hero="aboutHero">
+      <Banner title="About" subtitle="International Beach Resort" />
+    </Hero>
+  );
 }
